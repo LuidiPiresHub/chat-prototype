2f054050012aef0b0ea04715a10d7c02d93ef356
LuidiPires <luidihot@gmail.com>
2025-05-15 03:28:51 -0300

    prototype

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
 const clientUrl = 'http://localhost:3000';
 
 const app = express();
+app.use(express.json());
+app.use(cors({ origin: clientUrl }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, { 
    cors: {
     origin: clientUrl
   }
+ });
+
+ app.get('/', (_req: Request, res: Response): void => {
+  res.status(200).json({ message: 'Hello World!' });
  });
 
 io.on("connection", (socket) => {
